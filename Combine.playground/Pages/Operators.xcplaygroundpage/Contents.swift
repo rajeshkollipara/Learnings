64038709d7ec25033c874f4b60006a0dd25af4c6
rajeshkollipara <rajeshkollipara@users.noreply.github.com>
2020-10-08 23:58:00 +0530

    WIP: Filtering Operators

--- Combine.playground/Pages/Operators.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -154,3 +154,97 @@ example(of: "ignoreOutput") {
               receiveValue: { print($0) })
         .store(in: &subscriptions)
 }
+
+example(of: "first(where:)") {
+    let numbers = (1...9).publisher
+    numbers
+        .first(where: { $0 % 2 == 0 })
+        .sink(receiveCompletion: { print("Completed with: \($0)") },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "last(where:)") {
+    let numbers = (1...9).publisher
+    numbers
+        .last(where: { $0 % 2 == 0 })
+        .sink(receiveCompletion: { print("Completed with: \($0)") },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "last(where:) 2") {
+    let numbers = PassthroughSubject<Int, Never>()
+    
+    numbers
+        .last(where: { $0 % 2 == 0 })
+        .sink(receiveCompletion: { print("Completed with: \($0)") },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    numbers.send(1)
+    numbers.send(2)
+    numbers.send(3)
+    numbers.send(4)
+    numbers.send(5)
+    numbers.send(completion: .finished)
+    
+}
+
+example(of: "dropFirst") {
+    let numbers = (1...10).publisher
+    numbers
+        .dropFirst(8)
+        .sink(receiveValue: {
+            print($0)
+        })
+        .store(in: &subscriptions)
+}
+
+example(of: "drop(while:)") {
+    let numbers = (1...10).publisher
+    numbers
+        .drop(while: {
+            print("x")
+            return $0 % 5 != 0
+        }) .sink(receiveValue: {
+            print($0)
+        })
+        .store(in: &subscriptions)
+}
+
+example(of: "drop(untilOutputFrom:)") {
+    let isReady = PassthroughSubject<Void, Never>()
+    let taps = PassthroughSubject<Int, Never>()
+    taps
+        .drop(untilOutputFrom: isReady)
+        .sink(receiveValue: {
+            print($0)
+        })
+        .store(in: &subscriptions)
+    (1...5).forEach { n in
+        taps.send(n)
+        
+        if n == 3 {
+            isReady.send()
+        }
+    }
+}
+
+example(of: "prefix") {
+  let numbers = (1...10).publisher
+  numbers
+    .prefix(2)
+    .sink(receiveCompletion: { print("Completed with: \($0)") },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prefix(while:)") {
+  let numbers = (1...10).publisher
+  numbers
+    .prefix(while: { $0 < 3 })
+    .sink(receiveCompletion: { print("Completed with: \($0)") },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
