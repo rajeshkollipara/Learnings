8d524b5c0b020abd26b622585475bc49ba471dc2
rajeshkollipara <rajeshkollipara@users.noreply.github.com>
2020-10-24 09:36:31 +0530

    Finished Sequence Operators

--- Combine.playground/Pages/TimeShiftOperators.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/TimeShiftOperators.xcplaygroundpage/Contents.swift
@@ -36,7 +36,7 @@ PlaygroundPage.current.liveView = UIHostingController(rootView: view)
 sourcePublisher.displayEvents(in: sourceTimeline)
 delayedPublisher.displayEvents(in: delayedTimeline)
 */
-
+/*
 let valuesPerSecond = 1.0
 let collectTimeStride = 4
 let collectMaxCount = 2
@@ -66,3 +66,98 @@ PlaygroundPage.current.liveView = UIHostingController(rootView: view)
 sourcePublisher.displayEvents(in: sourceTimeline)
 collectedPublisher.displayEvents(in: collectedTimeline)
 collectedPublisher2.displayEvents(in: collectedTimeline2)
+*/
+
+/*
+public let typingHelloWorld: [(TimeInterval, String)] = [
+  (0.0, "H"),
+  (0.1, "He"),
+  (0.2, "Hel"),
+  (0.3, "Hell"),
+  (0.5, "Hello"),
+  (0.6, "Hello "),
+  (3.0, "Hello W"),
+  (3.1, "Hello Wo"),
+  (3.2, "Hello Wor"),
+  (3.4, "Hello Worl"),
+  (3.5, "Hello World")
+]
+
+let subject = PassthroughSubject<String, Never>()
+let debounced = subject
+    .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+  .share()
+let subjectTimeline = TimelineView(title: "Emitted values")
+let debouncedTimeline = TimelineView(title: "Debounced values")
+
+let view = VStack(spacing: 100) {
+    subjectTimeline
+    debouncedTimeline
+}
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: view)
+
+subject.displayEvents(in: subjectTimeline)
+debounced.displayEvents(in: debouncedTimeline)
+
+let subscription1 = subject
+  .sink { string in
+    print("+\(deltaTime)s: Subject emitted: \(string)")
+  }
+
+let subscription2 = debounced
+  .sink { string in
+    print("+\(deltaTime)s: Debounced emitted: \(string)")
+  }
+
+subject.feed(with: typingHelloWorld)
+
+*/
+
+public let typingHelloWorld: [(TimeInterval, String)] = [
+  (0.0, "H"),
+  (0.1, "He"),
+  (0.2, "Hel"),
+  (0.3, "Hell"),
+  (0.5, "Hello"),
+  (0.6, "Hello "),
+  (1.5, "Hello *"),
+  (2.0, "Hello W"),
+  (2.1, "Hello Wo"),
+  (2.2, "Hello Wor"),
+  (2.4, "Hello Worl"),
+  (2.5, "Hello World")
+]
+
+let throttleDelay = 1.0
+
+
+let subject = PassthroughSubject<String, Never>()
+let throttled = subject
+  .throttle(for: .seconds(throttleDelay), scheduler: DispatchQueue.main, latest: true)
+    .share()
+let subjectTimeline = TimelineView(title: "Emitted values")
+let throttledTimeline = TimelineView(title: "Throttled values")
+
+let view = VStack(spacing: 100) {
+    subjectTimeline
+    throttledTimeline
+}
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: view)
+
+subject.displayEvents(in: subjectTimeline)
+throttled.displayEvents(in: throttledTimeline)
+
+let subscription1 = subject
+  .sink { string in
+    print("+\(deltaTime)s: Subject emitted: \(string)")
+  }
+
+let subscription2 = throttled
+  .sink { string in
+    print("+\(deltaTime)s: Throttled emitted: \(string)")
+  }
+subject.feed(with: typingHelloWorld)
+
+
